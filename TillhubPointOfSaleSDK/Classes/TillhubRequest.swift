3c3d9552b2666cc7f30f9f5a927dbff26abb16e1
Andreas Hilbert <andreas.hilbert@tillhub.de>
2019-04-11 23:49:12 +0200

    reb

---
+++ TillhubPointOfSaleSDK/Classes/TillhubRequest.swift
@@ -0,0 +1,12 @@
+//
+//  TillhubRequest.swift
+//  TillhubPointOfSaleSDK
+//
+//  Created by Andreas Hilbert on 11.04.19.
+//
+
+import UIKit
+
+class TillhubRequest: NSObject {
+    let startingID = "randomStart"
+}
